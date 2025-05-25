fe005171b644e3b638693e74236f4d24a94396aa
Gabriel Gjini <gjini.gabriel@hotmail.com>
2025-05-25 14:46:44 +0200

    Add initial conditions and plot

--- src/common.h
+++ src/common.h
@@ -6,9 +6,9 @@
 
 struct vec3
 {
-    float x;
-    float y;
-    float z;
+    double x;
+    double y;
+    double z;
     vec3 operator+(const vec3& other) const
     {
         return {x + other.x, y + other.y, z + other.z};
@@ -17,11 +17,11 @@ struct vec3
     {
         return {x - other.x, y - other.y, z - other.z};
     }
-    vec3 operator*(float scalar) const
+    vec3 operator*(double scalar) const
     {
         return {x * scalar, y * scalar, z * scalar};
     }
-    vec3 operator/(float scalar) const
+    vec3 operator/(double scalar) const
     {
         return {x / scalar, y / scalar, z / scalar};
     }
@@ -43,14 +43,14 @@ struct vec3
         z -= other.z;
         return *this;
     }
-    vec3& operator*=(float scalar)
+    vec3& operator*=(double scalar)
     {
         x *= scalar;
         y *= scalar;
         z *= scalar;
         return *this;
     }
-    vec3& operator/=(float scalar)
+    vec3& operator/=(double scalar)
     {
         x /= scalar;
         y /= scalar;
@@ -60,7 +60,7 @@ struct vec3
 };
 
 // Allow scalar * vec3 multiplication
-inline vec3 operator*(float scalar, const vec3& v)
+inline vec3 operator*(double scalar, const vec3& v)
 {
     return {v.x * scalar, v.y * scalar, v.z * scalar};
 }
@@ -68,14 +68,14 @@ inline vec3 operator*(float scalar, const vec3& v)
 class System
 {
 public:
-    System(int numParticles, const std::vector<vec3>& x, const std::vector<vec3>& v, const std::vector<float>& m, float G) 
+    System(int numParticles, const std::vector<vec3>& x, const std::vector<vec3>& v, const std::vector<double>& m, double G) 
         : m_numParticles(numParticles), m_x(x), m_v(v), m_m(m), m_G(G) {}
 
     void correctCenterOfMass(void)
     {
         vec3 xcm;
         vec3 vcm;
-        float M = 0;
+        double M = 0;
         for (int i = 0; i < m_numParticles; ++i)
         {
             xcm += m_m[i]*m_x[i];
@@ -94,8 +94,8 @@ public:
     int m_numParticles;
     std::vector<vec3> m_x;
     std::vector<vec3> m_v;
-    std::vector<float> m_m;
-    float m_G;
+    std::vector<double> m_m;
+    double m_G;
 };
 
 namespace plt = matplotlibcpp;
@@ -103,21 +103,18 @@ namespace plt = matplotlibcpp;
 void plotInitialConditions(
     const System& system, 
     const std::vector<std::string>& colors, 
+    std::vector<std::string>& labels,
     bool legend = true,
     const std::string& filename = "") {
     // Set axis labels with LaTeX-style formatting
     plt::xlabel("$x$ (AU)");
     plt::ylabel("$y$ (AU)");
 
-    std::vector<float> x_coords;
-    std::vector<float> y_coords;
-    std::vector<std::string> point_colors;
     for (size_t i = 0; i < system.m_numParticles; ++i) {
-        x_coords.push_back(system.m_x[i].x);
-        y_coords.push_back(system.m_x[i].y);
-        point_colors.push_back(colors[i]);
+        std::vector<double> x = {static_cast<double>(system.m_x[i].x)};
+        std::vector<double> y = {static_cast<double>(system.m_x[i].y)};
+        plt::scatter(x, y, 10.0, { {"color", colors[i]}, {"label", labels[i]} });
     }
-    plt::scatter_colored(x_coords, y_coords, point_colors);
     if (legend) {
         plt::legend();
     }

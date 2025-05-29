c560b576588e61d031f02d752a168bf3f64c3ec2
Gabriel Gjini <gjini.gabriel@hotmail.com>
2025-05-29 18:07:40 +0200

    Add some vec functionality

--- src/vec3.h
+++ src/vec3.h
@@ -7,6 +7,8 @@ struct vec3
     double x;
     double y;
     double z;
+    vec3(double x = 0.0, double y = 0.0, double z = 0.0) : x(x), y(y), z(z) {}
+    vec3(const vec3& other) : x(other.x), y(other.y), z(other.z) {}
     vec3 operator+(const vec3& other) const
     {
         return {x + other.x, y + other.y, z + other.z};
@@ -61,4 +63,9 @@ struct vec3
 inline vec3 operator*(double scalar, const vec3& v)
 {
     return {v.x * scalar, v.y * scalar, v.z * scalar};
+}
+
+inline double norm(const vec3& v)
+{
+    return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
 }
